01bd849a747149c88697a5a6bd725d9d2e105fde
liewxinyee <61421698+liewxinyee@users.noreply.github.com>
2022-01-17 21:30:57 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import hydralit_components as hc
+import mode
+
+st.set_page_config(page_title="Crop Recommendation System🌱",layout='wide',initial_sidebar_state='collapsed',)
+
+# specify the primary menu definition
+menu_data = [
+    {'icon': "fa fa-tree", 'label':"Crop Recommendation"},
+    {'icon': "far fa-copy", 'label':"Overview"},
+    {'icon': "fas fa-info-circle", 'label':"About"},
+]
+
+over_theme = {'txc_inactive': '#FFFFFF','menu_background':'#62BD69'}
+menu_id = hc.nav_bar(
+    menu_definition=menu_data,
+    override_theme=over_theme,
+    home_name='Home',
+    hide_streamlit_markers=False, #will show the st hamburger as well as the navbar now!
+    sticky_nav=True, #at the top or not
+    sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
+)
+
+if (f"{menu_id}") == "Home":
+    mode.home()
+elif (f"{menu_id}") == "Crop Recommendation":
+    mode.envFac()
+elif (f"{menu_id}") == "Overview":
+    mode.selectCrop()
+    mode.graphs()
+elif (f"{menu_id}") == "About":
+    mode.about()
---
+++ mode.py
@@ -0,0 +1,94 @@
+from PIL import Image
+from models import recommend
+from selectbox import SelectCrop
+import crops
+import streamlit as st
+import pandas as pd
+
+def home():
+    st.title('Crop Recommendation System ')
+    st.markdown("""
+            ### This app recommends the most suitable crop for you!👨‍🌾 
+            """)
+    df = pd.read_csv(r'C:\Users\xy\Desktop\CropRecommendation_app\CropRecommendation_Cleaned.csv')
+    with st.expander("Click to See The Dataset Overview"):
+        col1, col2, col3 = st.columns([6, 1, 2])
+        with col1:
+            st.dataframe(df)
+        with col2:
+            st.write()
+        with col3:
+            st.metric(label="Total Crops", value = 22)
+    image = Image.open('background.jpg')
+    st.image(image, use_column_width=True)
+
+
+def envFac():
+    st.markdown("""
+            ### Instructions: Key in the following values and click the 'Predict' button
+            """)
+    recommend()
+
+def graphs():
+    nitrogen = Image.open('nitrogen.jpg')
+    phosphorus = Image.open('phosphorus.jpg')
+    potassium = Image.open('potassium.jpg')
+    weather = Image.open('weather.jpg')
+    with st.expander("Click to See The Comparison for Each Crop"):
+        type = st.radio("Select A Factor",
+                        ('Nitrogen(N)','Phosphorus(P)','Potassium(K)','Weather Factors'))
+        if type == 'Nitrogen(N)':
+            st.image(nitrogen,use_column_width=True)
+        elif type == 'Phosphorus(P)':
+            st.image(phosphorus, use_column_width=True)
+        elif type == 'Potassium(K)':
+            st.image(potassium, use_column_width=True)
+        elif type == 'Weather Factors':
+            st.image(weather, use_column_width=True)
+
+
+def selectCrop():
+    crop = SelectCrop()
+    crop.add_app("Apple", crops.apple)
+    crop.add_app("Banana", crops.banana)
+    crop.add_app("Blackgram", crops.blackgram)
+    crop.add_app("Chickpea", crops.chickpea)
+    crop.add_app("Coconut", crops.coconut)
+    crop.add_app("Coffee", crops.coffee)
+    crop.add_app("Cotton", crops.cotton)
+    crop.add_app("Grapes", crops.grapes)
+    crop.add_app("Jute", crops.jute)
+    crop.add_app("Kidneybeans", crops.kidneybeans)
+    crop.add_app("Lentil", crops.lentil)
+    crop.add_app("Maize", crops.maize)
+    crop.add_app("Mango", crops.mango)
+    crop.add_app("Mothbeans", crops.mothbeans)
+    crop.add_app("Mungbean", crops.mungbean)
+    crop.add_app("Muskmelon", crops.muskmelon)
+    crop.add_app("Orange", crops.orange)
+    crop.add_app("Papaya", crops.papaya)
+    crop.add_app("Pigeonpeas", crops.pigeonpeas)
+    crop.add_app("Pomegranate", crops.pomegranate)
+    crop.add_app("Rice", crops.rice)
+    crop.add_app("Watermelon", crops.watermelon)
+    crop.run()
+
+
+
+def about():
+    st.header("Crop Recommendation System🌱")
+    st.subheader("About This Application🍃")
+    st.write("This app takes environmental factors including temperature, humidity, rainfall and soil parameters which are the amount of nitrate, phosphorus, calcium and pH value of the soil into consideration to recommend the most suitable crop to the users.")
+    st.write("It aims to recommend the most suitable crop according to the given soil factors and climate conditions to the users")
+    st.subheader("Documentation📄")
+    st.write("Detailed documentation can be found at https://docs.google.com/document/d/12i0CliDVGxJXOemy-x431X-SMP-JRU3jGanCs3CnzGY/edit?usp=sharing")
+    st.subheader("Dataset Information📊")
+    st.write("The dataset used is obtained from Kaggle https://www.kaggle.com/atharvaingle/crop-recommendation-dataset")
+    st.subheader("Source Code👇")
+    st.write("Complete source code can be accessed at https://github.com/liewxinyee/Crop-Recommendation-System")
+    st.subheader("About Author🙎")
+    st.write("Hello! I am Liew Xin Yee from Malaysia, currently a thrid year Data Science student from University of Malaya.")
+    st.write("If you have any inquiries or feedback about this project, feel free to contact me via my email xiinyeeliew@gmail.com✉️")
+    st.markdown("""
+      ######  &nbsp [![this is an image link](https://camo.githubusercontent.com/d7b9f7e3f8af9348678c5042440844da48b892fb320482f313d28366d10c25d5/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c696e6b6564496e2d2532333030373742352e7376673f267374796c653d666f722d7468652d6261646765266c6f676f3d6c696e6b6564696e266c6f676f436f6c6f723d7768697465)](https://www.linkedin.com/in/liew-xin-yee-65a534214/)
+      """, unsafe_allow_html=True)
